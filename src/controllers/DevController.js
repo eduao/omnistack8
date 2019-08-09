8e4a1a7037f07bac18b1f116a217723fdcf05dcd
eduao <emaildoedu00@gmail.com>
2019-08-08 23:50:56 -0300

    storing dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,28 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+
 module.exports = {
-    store(req, res) {
-        return res.json({teste: true});
+    async store(req, res) {
+        const { username } = req.body
+        const url = `https://api.github.com/users/${username}`;
+        const responseGitApi = await axios.get(url);
+        const { name, bio, avatar_url : avatar } = responseGitApi.data;
+
+        const hasCreated = await Dev.findOne({
+            user: username
+        });
+
+        if (hasCreated) {
+            return res.json( hasCreated );
+        }
+
+        const dev = await Dev.create({
+            name, 
+            user: username, 
+            bio, 
+            avatar
+        });
+
+        return res.json(dev);
     }
 };
