1042ed5e9e5317e17fe128a5bcf07bc2d21a1d7a
eduao <emaildoedu00@gmail.com>
2019-08-11 10:46:33 -0300

    inserindo dislike function

---
+++ src/controllers/DisikeController.js
@@ -0,0 +1,41 @@
+const Dev = require("../models/Dev")
+
+module.exports = {
+    async store (req, res) {
+
+        const { targetDevId } = req.params;
+        const { fromdevid: fromDevId } = req.headers;
+
+        // Aqui tenho o dev alvo
+        const targetDev = await Dev.findById(targetDevId);
+        // Dev que deu like
+        const fromDev = await Dev.findById(fromDevId);
+
+        // Tratamento se não tiver o dev alvo cadastrado
+        if (! targetDev) {
+            return res.status(400)
+                .json({error: 'Target doesn`t exists'})
+        }
+
+        // Tratamento se não tiver o dev que quer dar like cadastrado
+        if (! fromDev) {
+            return res.status(400)
+                .json({error: 'Logged dev doesn`t exists'})
+        }
+
+        if (fromDev.dislikes.includes(targetDevId) ) {
+            return res.status(400)
+                .json({message: 'Are you a hacker?'})
+        }
+
+        fromDev.dislikes.push(targetDevId);
+
+        await fromDev.save();
+        // Caso eu tiver os dois salvo na lista 
+        res.json([
+            {message: 'sucesso!!'},
+            targetDev,
+            fromDev
+        ]);
+    }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({item: 'oi'});
 });
 
+routes.post('/devs/index', DevController.index);
 routes.post('/devs', DevController.store);
+routes.post('/devs/:targetDevId/dislikes', DislikeController.store);
 routes.post('/devs/:targetDevId/likes', LikeController.store);
 
 module.exports = routes;
