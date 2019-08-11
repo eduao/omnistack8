b06c8f2da528aeb825d40a293b4a3a1f93a6c5f6
eduao <emaildoedu00@gmail.com>
2019-08-11 10:46:58 -0300

    Listagem de dev disponíveis para o usuário logado avaliar

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,6 +2,26 @@ const axios = require("axios");
 const Dev = require("../models/Dev");
 
 module.exports = {
+
+    async index (req, res) {
+        const {  fromdevid: user } = req.headers;
+
+        if (! user) {
+            res.status(400)
+                .json({message: 'Dev not logged in'})
+        }
+
+        const loggedDev = await Dev.findById(user);
+        const devList = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedDev.likes }},
+                { _id: { $nin: loggedDev.dislikes }}
+            ]
+        });
+        res.json(devList);
+    },
+
     async store(req, res) {
         const { username } = req.body
         const url = `https://api.github.com/users/${username}`;
